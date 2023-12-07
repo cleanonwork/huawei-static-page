bb530fca217925d1b9f56ee44a3c72908a267d39
jademary <46882945+cleanonwork@users.noreply.github.com>
2023-12-07 16:39:09 +0800

    Update checkDomain.js

--- api/checkDomain.js
+++ api/checkDomain.js
@@ -1,1 +1,16 @@
+// /api/checkDomain.js
+
+module.exports = async (req, res) => {
+  const { name, suffix } = req.query;
+  const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+};
 
